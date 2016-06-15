62638facd0ffe06d6f942f4151b97506d39f982e
Fatih Nayebi <f.nayebi@gmail.com>
2016-06-15 01:26:49 -0400

    Cocoapods update
    Cocoapods dependency updates

--- Ch11-Frontend/TodoApp/Pods/Target Support Files/Pods-TodoAppTests/Pods-TodoAppTests-frameworks.sh
+++ Ch11-Frontend/TodoApp/Pods/Target Support Files/Pods-TodoAppTests/Pods-TodoAppTests-frameworks.sh
@@ -82,20 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Argo/Argo.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Curry/Curry.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Delta/Delta.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Argo/Argo.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Curry/Curry.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Delta/Delta.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
-fi
