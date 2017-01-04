a30be119f31f0330a7515cf59f93565a036c9eab
Fatih Nayebi <Fatih.NAYEBI@bdc.ca>
2017-01-04 12:11:08 -0500

    Second edition Chapter 5 playground changes

--- Playgrounds/Ch05-Generics.playground/Pages/GenericDataStructures.xcplaygroundpage/Contents.swift
+++ Playgrounds/Ch05-Generics.playground/Pages/GenericDataStructures.xcplaygroundpage/Contents.swift
@@ -3,26 +3,27 @@
 import Foundation
 
 enum GenericTree <T> {
-    case empty
-    case leaf(T)
-    indirect case node(GenericTree, GenericTree)
+	case empty
+	case leaf(T)
+	indirect case node(GenericTree, GenericTree)
 }
 
-let ourGenericTree = GenericTree.node(GenericTree.leaf("First"), GenericTree.node(GenericTree.leaf("Second"), GenericTree.leaf("Third")))
-print(ourGenericTree)
+print(GenericTree.node(GenericTree.leaf("First"), GenericTree.node(GenericTree.leaf("Second"), GenericTree.leaf("Third"))))
+// prints node(GenericTree<Swift.String>.leaf("First"), GenericTree<Swift.String>.node(GenericTree<Swift.String>.leaf("Second"), GenericTree<Swift.String>.leaf("Third")))
+
 
 struct Queue<Element> {
-    private var elements = [Element]()
-    mutating func enQueue(newElement: Element) {
-        elements.append(newElement)
-    }
-    
-    mutating func deQueue() -> Element? {
-        guard !elements.isEmpty else {
-            return nil
-        }
-        return elements.remove(at: 0)
-    }
+	private var elements = [Element]()
+	mutating func enQueue(newElement: Element) {
+		elements.append(newElement)
+	}
+
+	mutating func deQueue() -> Element? {
+		guard !elements.isEmpty else {
+			return nil
+		}
+		return elements.remove(at: 0)
+	}
 }
 
 //: [Next](@next)
--- Playgrounds/Ch05-Generics.playground/Pages/TypeConstraints.xcplaygroundpage/Contents.swift
+++ Playgrounds/Ch05-Generics.playground/Pages/TypeConstraints.xcplaygroundpage/Contents.swift
@@ -3,11 +3,11 @@
 import Foundation
 
 protocol NumericType {
-    static func +(lhs: Self, rhs: Self) -> Self
-    static func -(lhs: Self, rhs: Self) -> Self
-    static func *(lhs: Self, rhs: Self) -> Self
-    static func /(lhs: Self, rhs: Self) -> Self
-    static func %(lhs: Self, rhs: Self) -> Self
+	static func +(lhs: Self, rhs: Self) -> Self
+	static func -(lhs: Self, rhs: Self) -> Self
+	static func *(lhs: Self, rhs: Self) -> Self
+	static func /(lhs: Self, rhs: Self) -> Self
+	static func %(lhs: Self, rhs: Self) -> Self
 }
 
 extension Double : NumericType { }
@@ -23,30 +23,27 @@ extension UInt16 : NumericType { }
 extension UInt32 : NumericType { }
 extension UInt64 : NumericType { }
 
-func calculate<T: NumericType>(a: T,
-               b: T,
-           funcA: (T, T) -> T,
-           funcB: (T) -> T) -> T {
-    
-    return funcA(funcB(a), funcB(b))
+func calculate<T: NumericType>(a: T, b: T, funcA: (T, T) -> T, funcB: (T) -> T) -> T {
+
+	return funcA(funcB(a), funcB(b))
 }
 
 func addTwoValues(a: Int, b: Int) -> Int {
-    return a + b
+	return a + b
 }
 
 func square(a: Int) -> Int {
-    return a * a
+	return a * a
 }
 
 print("The result of adding two squared values is: \(calculate(a: 2, b: 2, funcA: addTwoValues, funcB: square))") // prints “The result of adding two squared value is: 8”
 
 func format(a: String) -> String {
-    return "formatted \(a)"
+	return "formatted \(a)"
 }
 
 func appendStrings(a: String, b: String) -> String {
-    return a + b
+	return a + b
 }
 
 //print("The result is: \(calculate(a: "2", b: "2", funcA: appendStrings, funcB: format))") // Compile error - Cannot convert value of types '(String, b: String) -> String' to expected argument type'(_, _) -> _'
