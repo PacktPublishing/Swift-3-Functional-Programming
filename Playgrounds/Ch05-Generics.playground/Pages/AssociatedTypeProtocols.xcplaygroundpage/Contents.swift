a856fcb99bc79038708e97eff85711def378d599
Fatih Nayebi <Fatih.NAYEBI@bdc.ca>
2017-04-04 08:41:42 -0400

    2nd Edition - Generics changes
    Example for Associated Type Protocols is changed and Type Erasure is
    added.

--- Playgrounds/Ch05-Generics.playground/Pages/AssociatedTypeProtocols.xcplaygroundpage/Contents.swift
+++ Playgrounds/Ch05-Generics.playground/Pages/AssociatedTypeProtocols.xcplaygroundpage/Contents.swift
@@ -2,20 +2,71 @@
 
 import Foundation
 
-protocol Container {
-    associatedtype ItemType
-    mutating func append(item: ItemType)
+protocol CustomView {
+	associatedtype ViewType
+	func configure(view with: ViewType)
 }
 
-struct IntContainer: Container {
-    typealias ItemType = Int
-    mutating func append(item: ItemType) {
-        // append item to the container
-    }
+struct Button {}
+
+struct CustomEnabledButton: CustomView {
+	typealias ViewType = Button
+	func configure(view with: ViewType) {
+		// configure the view
+		print("Enabled button")
+	}
 }
 
-var container = IntContainer()
-container.append(item: 1)
-print(container)
+var customEnabledButton = CustomEnabledButton()
+customEnabledButton.configure(view: Button())
+print(customEnabledButton)
+
+struct CustomDisabledButton: CustomView {
+	typealias ViewType = Button
+	func configure(view with: ViewType) {
+		// configure the view
+		print("Disabled button")
+	}
+}
+
+// let customViews = [CustomSwitch(), CustomEnabledButton(), CustomDisabledButton()]
+
+/// Type Erasure
+
+// Type-erased CustomView
+struct AnyCustomView<ViewType>: CustomView {
+	private let _configure: (ViewType) -> Void
+
+	init<Base: CustomView>(_ base: Base) where ViewType == Base.ViewType {
+		_configure = base.configure
+	}
+
+	func configure(view with: ViewType) {
+		_configure(with)
+	}
+}
+
+let views = [AnyCustomView(CustomEnabledButton()), AnyCustomView(CustomDisabledButton())]
+
+let button = Button()
+
+
+for view in views {
+	view.configure(view: button)
+	print(view)
+}
+
+struct Switch {}
+
+struct CustomSwitch: CustomView {
+	typealias ViewType = Switch
+	func configure(view with: ViewType) {
+		// configure the view
+		print("Custom switch")
+	}
+}
+
+// let views = [AnyCustomView(CustomSwitch()), AnyCustomView(CustomDisabledButton())]
+// error: heterogeneous collection literal could only be inferred to '[Any]'; add explicit type annotation if this is intentional
 
 //: [Next](@next)
